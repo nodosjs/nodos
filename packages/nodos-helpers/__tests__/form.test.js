96278a6ed9b9f5952c9b46375d4b97bc52629179
greybutton <greybutton@yandex.com>
2018-10-28 19:07:03 +0800

    add submit, form

--- packages/nodos-helpers/__tests__/form.test.js
+++ packages/nodos-helpers/__tests__/form.test.js
@@ -1,9 +1,17 @@
-import { textFieldTag, labelTag } from '../src/form';
+import {
+  textFieldTag, labelTag, submitTag, form, Form,
+} from '../src/form';
 
 describe('text field tag', () => {
   test('default', () => {
     const actual = textFieldTag('title', 'Hello!');
     const expected = '<input id="title" name="title" type="text" value="Hello!">';
+    expect(actual).toEqual(expected);
+  });
+
+  test('without value', () => {
+    const actual = textFieldTag('title');
+    const expected = '<input id="title" name="title" type="text" value="">';
     expect(actual).toEqual(expected);
   });
 
@@ -78,6 +86,146 @@ describe('label tag', () => {
   test('id option', () => {
     const actual = labelTag('title', 'My title', { id: 'label' });
     const expected = '<label for="title" id="label">My title</label>';
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('submit tag', () => {
+  test('empty', () => {
+    const actual = submitTag();
+    const expected = '<input name="commit" type="submit" value="Save changes">';
+    expect(actual).toEqual(expected);
+  });
+
+  test('having data disable with string', () => {
+    const actual = submitTag('Save', { 'data-disable-with': 'Processing...', 'data-confirm': 'Are you sure?' });
+    const expected = '<input name="commit" type="submit" value="Save" data-disable-with="Processing..." data-confirm="Are you sure?">';
+    expect(actual).toEqual(expected);
+  });
+
+  test('having data disable with boolean', () => {
+    const actual = submitTag('Save', { 'data-disable-with': false, 'data-confirm': 'Are you sure?' });
+    const expected = '<input name="commit" type="submit" value="Save" data-confirm="Are you sure?">';
+    expect(actual).toEqual(expected);
+  });
+
+  test('having data hash disable with boolean', () => {
+    const actual = submitTag('Save', { data: { confirm: 'Are you sure?', disable_with: false } });
+    const expected = '<input name="commit" type="submit" value="Save" data-confirm="Are you sure?">';
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('form function', () => {
+  test('default', () => {
+    const actual = form({ url: '/search' });
+    const expected = '<form accept-charset="UTF-8" action="/search" data-remote="true" method="post"></form>';
+    expect(actual).toEqual(expected);
+  });
+  test('with params', () => {
+    const actual = form({ url: '/search', method: 'get' });
+    const expected = '<form accept-charset="UTF-8" action="/search" data-remote="true" method="get"></form>';
+    expect(actual).toEqual(expected);
+  });
+  test('with class', () => {
+    const actual = form({ url: '/search', class: 'admin' });
+    const expected = '<form accept-charset="UTF-8" action="/search" data-remote="true" method="post" class="admin"></form>';
+    expect(actual).toEqual(expected);
+  });
+  test('with html', () => {
+    const actual = form({
+      url: '/search',
+      method: 'get',
+      html: `<p>${labelTag('q', 'Search for:')}${textFieldTag('q')}</p><p>${submitTag('Search')}</p>`,
+    });
+    const expected = '<form accept-charset="UTF-8" action="/search" data-remote="true" method="get"><p><label for="q">Search for:</label><input id="q" name="q" type="text" value=""></p><p><input name="commit" type="submit" value="Search"></p></form>';
+    expect(actual).toEqual(expected);
+  });
+  test('html with class', () => {
+    const label = labelTag('q', 'Search for:', { class: 'admin' });
+    const input = textFieldTag('q', '', { class: 'admin' });
+    const submit = submitTag('Search', { class: 'admin' });
+    const actual = form({
+      url: '/search',
+      method: 'get',
+      class: 'admin',
+      html: `<p class="admin">${label}${input}</p><p>${submit}</p>`,
+    });
+    const expected = '<form accept-charset="UTF-8" action="/search" data-remote="true" method="get" class="admin"><p class="admin"><label for="q" class="admin">Search for:</label><input id="q" name="q" type="text" value="" class="admin"></p><p><input name="commit" type="submit" value="Search" class="admin"></p></form>';
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('form class', () => {
+  test('default', () => {
+    const f = new Form({ url: '/search' });
+    const actual = f.render();
+    const expected = '<form accept-charset="UTF-8" action="/search" data-remote="true" method="post"><input id="authenticity_token" name="authenticity_token" type="hidden" value="key"></form>';
+    expect(actual).toEqual(expected);
+  });
+  test('with params', () => {
+    const f = new Form({ url: '/search', method: 'get' });
+    const actual = f.render();
+    const expected = '<form accept-charset="UTF-8" action="/search" data-remote="true" method="get"><input id="authenticity_token" name="authenticity_token" type="hidden" value="key"></form>';
+    expect(actual).toEqual(expected);
+  });
+  test('with class', () => {
+    const f = new Form({ url: '/search', class: 'admin' });
+    const actual = f.render();
+    const expected = '<form accept-charset="UTF-8" action="/search" data-remote="true" method="post" class="admin"><input id="authenticity_token" name="authenticity_token" type="hidden" value="key"></form>';
+    expect(actual).toEqual(expected);
+  });
+  test('with html', () => {
+    const f = new Form({
+      url: '/search',
+      method: 'get',
+    });
+    const html = `<p>${f.label('q', 'Search for:')}${f.textField('q')}</p><p>${f.submit('Search')}</p>`;
+    const actual = f.render(html);
+    const expected = '<form accept-charset="UTF-8" action="/search" data-remote="true" method="get"><input id="authenticity_token" name="authenticity_token" type="hidden" value="key"><p><label for="q">Search for:</label><input id="q" name="q" type="text" value=""></p><p><input name="commit" type="submit" value="Search"></p></form>';
+    expect(actual).toEqual(expected);
+  });
+  test('form with scope', () => {
+    const f = new Form({
+      url: '/posts',
+      scope: 'post',
+    });
+    const html = `${f.textField('title')}`;
+    const actual = f.render(html);
+    const expected = '<form accept-charset="UTF-8" action="/posts" data-remote="true" method="post"><input id="authenticity_token" name="authenticity_token" type="hidden" value="key"><input id="post[title]" name="post[title]" type="text" value=""></form>';
+    expect(actual).toEqual(expected);
+  });
+  test('html with class', () => {
+    const f = new Form({
+      url: '/search',
+      scope: 'post',
+      method: 'get',
+      class: 'admin',
+    });
+    const label = f.label('q', 'Search for:', { class: 'admin' });
+    const input = f.textField('q', '', { class: 'admin' });
+    const submit = f.submit('Search', { class: 'admin' });
+    const html = `<p class="admin">${label}${input}</p><p>${submit}</p>`;
+    const actual = f.render(html);
+    const expected = '<form accept-charset="UTF-8" action="/search" data-remote="true" method="get" class="admin"><input id="authenticity_token" name="authenticity_token" type="hidden" value="key"><p class="admin"><label for="post[q]" class="admin">Search for:</label><input id="post[q]" name="post[q]" type="text" value="" class="admin"></p><p><input name="commit" type="submit" value="Search" class="admin"></p></form>';
+    expect(actual).toEqual(expected);
+  });
+  test('method patch', () => {
+    const f = new Form({ url: '/search', method: 'patch' });
+    const actual = f.render();
+    const expected = '<form accept-charset="UTF-8" action="/search" data-remote="true" method="post"><input id="_method" name="_method" type="hidden" value="patch"><input id="authenticity_token" name="authenticity_token" type="hidden" value="key"></form>';
+    expect(actual).toEqual(expected);
+  });
+  test('method put', () => {
+    const f = new Form({ url: '/search', method: 'put' });
+    const actual = f.render();
+    const expected = '<form accept-charset="UTF-8" action="/search" data-remote="true" method="post"><input id="_method" name="_method" type="hidden" value="put"><input id="authenticity_token" name="authenticity_token" type="hidden" value="key"></form>';
+    expect(actual).toEqual(expected);
+  });
+  test('method delete', () => {
+    const f = new Form({ url: '/search', method: 'delete' });
+    const actual = f.render();
+    const expected = '<form accept-charset="UTF-8" action="/search" data-remote="true" method="post"><input id="_method" name="_method" type="hidden" value="delete"><input id="authenticity_token" name="authenticity_token" type="hidden" value="key"></form>';
     expect(actual).toEqual(expected);
   });
 });
--- packages/nodos-helpers/src/form.js
+++ packages/nodos-helpers/src/form.js
@@ -1,10 +1,14 @@
 import tag from './tags';
 
-export const textFieldTag = (name = null, value = null, options = {}) => {
+export const textFieldTag = (name = null, value = '', options = {}) => {
   const config = {
     name: 'input',
     attributes: {
-      id: name, name, type: 'text', value, ...options,
+      id: name,
+      name,
+      type: 'text',
+      value,
+      ...options,
     },
   };
   const result = tag(config);
@@ -15,10 +19,95 @@ export const labelTag = (name = 'title', content = 'Title', options = {}) => {
   const config = {
     name: 'label',
     attributes: {
-      for: name, ...options,
+      for: name,
+      ...options,
     },
     text: content,
   };
   const result = tag(config);
   return result;
 };
+
+export const submitTag = (value = 'Save changes', options = {}) => {
+  const { data, ...rest } = options;
+  const newData = data && Object.keys(data).reduce((acc, key) => {
+    const newAcc = { ...acc, [`data-${key}`]: data[key] };
+    return newAcc;
+  }, {});
+  const config = {
+    name: 'input',
+    attributes: {
+      name: 'commit',
+      type: 'submit',
+      value,
+      ...newData,
+      ...rest,
+    },
+  };
+  const result = tag(config);
+  return result;
+};
+
+export const form = (options = {}) => {
+  const {
+    url, method = 'post', html = '', ...rest
+  } = options;
+  const config = {
+    name: 'form',
+    attributes: {
+      'accept-charset': 'UTF-8',
+      action: url,
+      'data-remote': 'true',
+      method,
+      ...rest,
+    },
+    html,
+  };
+  const result = tag(config);
+  return result;
+};
+
+export class Form {
+  constructor(options = {}) {
+    const {
+      scope, ...rest
+    } = options;
+    this.scope = scope;
+    this.rest = rest;
+  }
+
+  render(html = '') {
+    const { method = 'post' } = this.rest;
+    let methodInput = '';
+    let newMethod = method;
+    if (method !== 'get' && method !== 'post') {
+      methodInput = textFieldTag('_method', method, { type: 'hidden' });
+      newMethod = 'post';
+    }
+    const key = 'key';
+    const hidden = textFieldTag('authenticity_token', key, { type: 'hidden' });
+    const newHtml = `${methodInput}${hidden}${html}`;
+    return form({ ...this.rest, method: newMethod, html: newHtml });
+  }
+
+  textField(name, value, options) {
+    if (this.scope) {
+      const newName = `${this.scope}[${name}]`;
+      return textFieldTag(newName, value, options);
+    }
+    return textFieldTag(name, value, options);
+  }
+
+  label(name, content, options) {
+    if (this.scope) {
+      const newName = `${this.scope}[${name}]`;
+      return labelTag(newName, content, options);
+    }
+    return labelTag(name, content, options);
+  }
+
+  // eslint-disable-next-line
+  submit(value, options) {
+    return submitTag(value, options);
+  }
+}
