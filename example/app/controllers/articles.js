cd4c6f3e19d2548d5e97cf85e18c5995950ac061
Alex Shakun <alx.safari@gmail.com>
2018-11-05 19:54:53 +0100

    add name to example package json because of yeoman

---
+++ example/app/controllers/articles.js
@@ -0,0 +1,18 @@
+
+export const index = (request, response) => {
+};
+
+export const build = (request, response) => {
+};
+
+export const edit = (request, response) => {
+};
+
+export const show = (request, response) => {
+};
+
+export const create = (request, response) => {
+};
+
+export const destroy = (request, response) => {
+};
