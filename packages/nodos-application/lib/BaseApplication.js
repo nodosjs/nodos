25e76b10a516bff0072d2ce7c7dfa2bbd80a32c7
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2018-11-21 04:45:26 +0500

    add router as dependency

--- packages/nodos-application/lib/BaseApplication.js
+++ packages/nodos-application/lib/BaseApplication.js
@@ -62,6 +62,7 @@ export default class BaseApplication {
   async start() {
     await this.init();
     this.router = await buildRouter(this.config.paths.routes, { host: this.config.host });
+    this.addDependency('router', this.router);
     await Promise.all(this.extensions.map(([f, options]) => f(this, options)));
     // TODO: pass only data, not application
     this.fastify = await buildFastify(this);
