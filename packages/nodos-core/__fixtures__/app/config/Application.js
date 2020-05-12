4d3f6914471c7b1ae1290c2f1a0db5c066b3dda4
Kirill Mokevnin <mokevnin@gmail.com>
2020-05-11 23:31:26 -0400

    fix tests

--- packages/nodos-core/__fixtures__/app/config/Application.js
+++ packages/nodos-core/__fixtures__/app/config/Application.js
@@ -1,10 +1,7 @@
 import { BaseApplication } from '../../../lib';
-import fastifyCookie from 'fastify-cookie';
-import fastifyFormbody from 'fastify-formbody';
 
 export default class Application extends BaseApplication {
   async init() {
-    this.addPlugin(fastifyCookie);
-    this.addPlugin(fastifyFormbody);
+    // init
   }
 }
