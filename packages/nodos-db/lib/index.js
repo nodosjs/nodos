3da8ddc24f98a0081bbb0a62ad2f757309767733
Kirill Mokevnin <mokevnin@gmail.com>
2020-05-11 22:50:43 -0400

    fix db

--- packages/nodos-db/lib/index.js
+++ packages/nodos-db/lib/index.js
@@ -6,6 +6,6 @@ module.exports = async (app) => {
   const db = new Db(app.config.db);
   await db.connect();
   Object.values(commands).forEach((command) => app.addCommand(command));
-  // app.addDependency('db', db);
-  // app.addHook('onStop', db.close);
+  app.addDependency('db', db);
+  app.addHook('onStop', db.close);
 };
