23a817c6b5ee42e4ff04b1d79f2eab4790b95a73
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2020-05-17 00:14:43 +0500

    generate templates(views) with controllers, but I don't like implementation of type condition, better way should be found later

--- packages/nodos-cli/lib/generator/tasks.js
+++ packages/nodos-cli/lib/generator/tasks.js
@@ -16,9 +16,22 @@ module.exports = class Generator extends Base {
   writing() {
     const { type, name } = this.options;
 
-    this.fs.copy(
-      this.templatePath(`${type}.js`),
-      this.destinationPath(`./app/${type}s/${name}s.js`),
-    );
+    // TODO: don't like switch case statement here, should be refactored to method dispatching
+    // But Generator class is simply calls all custom methods one by one regardless of any condition
+    switch (type) {
+      case 'controller':
+        this.fs.copy(
+          this.templatePath(`controller.js`),
+          this.destinationPath(`./app/controllers/${name}s.js`),
+        );
+
+        this.fs.copy(
+          this.templatePath(`templates/`),
+          this.destinationPath(`./app/templates/${name}s/`),
+        );
+        break;
+      default:
+        break;
+    }
   }
 };
