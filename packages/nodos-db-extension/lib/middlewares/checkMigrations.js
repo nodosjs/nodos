0061389308e9cab93949dc0eedaa4a8df0b720a2
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2020-08-24 03:02:06 +0500

    add checkMigrations middleware, small fixes in example's templates

--- packages/nodos-db-extension/lib/middlewares/checkMigrations.js
+++ packages/nodos-db-extension/lib/middlewares/checkMigrations.js
@@ -1,1 +1,17 @@
-module.exports = (action) => action();
+const knex = require('knex');
+
+module.exports = async (action, _request, response, { container }) => {
+  const knexClient = knex(container.db.config);
+  const [, pendingMigraions] = await knexClient.migrate.list();
+  if (pendingMigraions.length > 0) {
+    const migrationsList = pendingMigraions.map((m) => `- ${m.file}`);
+    const message = [
+      'Here is a list of pending migrations:\n',
+      ...migrationsList,
+    ].join('\n');
+
+    response.head(404, message);
+  }
+
+  await action();
+};
