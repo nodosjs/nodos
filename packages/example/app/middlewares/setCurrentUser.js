d720edfee125a3f73a5633834d39df03230d7f16
mokevnin <mokevnin@gmail.com>
2020-08-23 09:24:30 -0400

    update example

--- packages/example/app/middlewares/setCurrentUser.js
+++ packages/example/app/middlewares/setCurrentUser.js
@@ -1,7 +1,7 @@
 import User from '../entities/User';
 import Guest from '../entities/Guest';
 
-export default async (request, _response) => {
+export default async (request) => {
   const { userId } = request.session;
   const currentUser = userId
     ? await User.query().findById(userId)
