4ee1e385ca83fc73ba850c239ee37e7170955a7f
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2020-08-31 01:24:52 +0500

    fix example to use nodosjs/webpacker and clean dependencies

--- packages/example/config/application.js
+++ packages/example/config/application.js
@@ -1,6 +1,8 @@
 import buildNodosDbExtension from '@nodosjs/db-extension';
+import webpacker from '@nodosjs/webpacker';
 
 export default async (app) => {
   const db = await buildNodosDbExtension(app.config.db);
   app.addExtension(db);
+  app.addExtension(webpacker);
 };
