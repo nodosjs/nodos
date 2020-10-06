f7ee7f61f46f70a2181a24ccfd7ba4eac6ebe66f
Eugene Sinitsyn <sinitsyn.e@n-t.io>
2020-10-06 13:09:55 +0300

    Add relation to user

--- packages/example/app/entities/Post.js
+++ packages/example/app/entities/Post.js
@@ -1,5 +1,6 @@
 import { Model } from 'objection';
 import Password from 'objection-password';
+import User from './User'
 
 export default class Post extends Model {
   static get tableName() {
@@ -7,18 +8,25 @@ export default class Post extends Model {
   }
 
   static get jsonSchema() {
-      return {
-        type: 'object',
-        required: ['title', 'text'],
-        properties: {
-          id: { type: 'integer' },
-          title: { type: 'string' },
-          text: { type: 'text', minLength: 50 },
-        },
-      };
-    }
-
-  get isGuest() {
-    return false;
+    return {
+      type: 'object',
+      required: ['title', 'text'],
+      properties: {
+        id: { type: 'integer' },
+        title: { type: 'string' },
+        text: { type: 'text', minLength: 50 },
+      },
+    };
   }
+
+  static relationMappings = {
+    author: {
+      relation: Model.BelongsToOneRelation,
+      modelClass: User,
+      join: {
+        from: 'users.id',
+        to: 'posts.author_id'
+      }
+    }
+  };
 }
