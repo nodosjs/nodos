ec3398d808de8e23e01bb5f06cb57d32e366fe8e
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2020-10-09 22:46:24 +0500

    proxy only missed methods

--- packages/nodos-core/lib/http/Request.js
+++ packages/nodos-core/lib/http/Request.js
@@ -2,7 +2,57 @@
 
 class Request {
   constructor(fastifyRequest) {
-    return new Proxy(fastifyRequest, {});
+    this.fastifyRequest = fastifyRequest;
+
+    return new Proxy(this, {
+      get: function(request, prop) {
+        if (prop in request) {
+          return request[prop];
+        }
+
+        return request.fastifyRequest[prop];
+      }
+    })
+  }
+
+  get body() {
+    return this.fastifyRequest.body;
+  }
+
+  get query() {
+    return this.fastifyRequest.query;
+  }
+
+  get headers() {
+    return this.fastifyRequest.headers;
+  }
+
+  get params() {
+    return this.fastifyRequest.params;
+  }
+
+  get ip() {
+    return this.fastifyRequest.ip;
+  }
+
+  get hostname() {
+    return this.fastifyRequest.hostname;
+  }
+
+  get method() {
+    return this.fastifyRequest.method;
+  }
+
+  get url() {
+    return this.fastifyRequest.url;
+  }
+
+  get routeMethod() {
+    return this.fastifyRequest.routeMethod;
+  }
+
+  get connection() {
+    return this.fastifyRequest.connection;
   }
 }
 
