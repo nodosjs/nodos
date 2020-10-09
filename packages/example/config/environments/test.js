778f6078b7deb8f4a14f7871d2f3d2e64264e056
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2020-10-09 23:29:45 +0500

    turn off csrf in test env, add webpack extension only in dev env

--- packages/example/config/environments/test.js
+++ packages/example/config/environments/test.js
@@ -1,5 +1,4 @@
 export default async (app) => {
   app.config.logLevel = 'debug';
   app.config.cacheModules = true;
-  app.config.csrfOptions = { ignoreMethods: ['HEAD', 'OPTIONS', 'GET', 'POST', 'PUT', 'PATCH'] };
 };
