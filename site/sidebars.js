f07304f263ad59aa9f427409e8c49c08a5fd8c03
Basil <morphizm@ya.ru>
2020-10-14 03:11:24 +0300

    simple doc for i18next

--- site/sidebars.js
+++ site/sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
     'Getting Started': ['getting_started', 'motivation'],
+    'More': ['i18next'],
     'Simple examples with docusaurus': ['doc1', 'mdx'], // I don't delete this, because it contain some hints to work with doc.
   },
 };
