27a3621566386ffa31ac0353b7463cacacc95849
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2020-10-16 02:32:21 +0500

    add comment to explain why we are using custom compiler

--- packages/nodos-webpack-extension/index.js
+++ packages/nodos-webpack-extension/index.js
@@ -6,6 +6,8 @@ module.exports = (app) => {
   if (app.isDevelopment()) {
     const webpackConfigPath = path.join(app.config.paths.configPath, 'webpack/development.js');
     const webpackConfig = require(webpackConfigPath).default; // eslint-disable-line
+    // We are using our own compiler instead of passing webpack config
+    // since current fastify-webpack-hmr does not support webpack v5
     const compiler = webpack(webpackConfig);
 
     app.addPlugin(fastifyWebpackHMR, { compiler });
