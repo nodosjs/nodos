c61bea9e77ebb9c9797a1c40bf4d87b7c8344be6
Uunnamed <uunnamed@mail.ru>
2020-10-25 23:33:47 +0300

    fix csrf config in example

--- packages/example/config/environments/development.js
+++ packages/example/config/environments/development.js
@@ -6,6 +6,7 @@ export default async (app) => {
 
   app.config.logLevel = 'debug';
   app.config.cacheModules = false;
+  app.config.csrf = { enabled: false }
   app.config.db = {
     client: 'sqlite3',
     connection: 'db/development.sqlite3',
--- packages/example/config/environments/production.js
+++ packages/example/config/environments/production.js
@@ -1,4 +1,5 @@
 export default async (app) => {
   app.config.logLevel = 'info';
   app.config.cacheModules = true;
+  app.config.csrf = { enabled: false }
 }
