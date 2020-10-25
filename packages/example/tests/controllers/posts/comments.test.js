37dc6a77507cb39282ff080a5aa4b69d2152dbc6
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2020-10-25 15:43:52 +0500

    add parent route names to resource name to fix controller path for nested routes

--- packages/example/tests/controllers/posts/comments.test.js
+++ packages/example/tests/controllers/posts/comments.test.js
@@ -1,2 +1,5 @@
 test('example/posts/comments', async () => {
+  const post = { id: 1 }
+  const response = await app.get(`/posts/${post.id}/comments`);
+  expect(response).toMatchObject({ statusCode: 200 });
 });
