824a967dc105c581695da7a443de778a006fcda6
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2020-11-08 22:37:55 +0500

    check node version before running nodos commands

--- packages/nodos-cli/index.js
+++ packages/nodos-cli/index.js
@@ -1,4 +1,5 @@
 const importFrom = require('import-from');
+const semver = require('semver')
 const runCurrent = require('./lib/current.js');
 const runNew = require('./lib/new.js');
 const log = require('./lib/logger.js');
@@ -7,6 +8,9 @@ module.exports = {
   runNew,
   runCurrent,
   run(dir, options = {}) {
+    if (semver.lt(process.versions.node, '14.0.0')) {
+      throw new Error('You need at least Node v14 to work with nodos');
+    }
     const core = importFrom.silent(dir, '@nodosjs/core');
     log(dir);
     log(core);
