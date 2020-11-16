d7f89fe68a5e98af24409075cee2d0bbcc10befb
Artem Arefiev <artem.arefiev@uchi.ru>
2020-11-16 11:23:21 +0400

    update tests

--- packages/nodos-routing/__tests__/index.test.js
+++ packages/nodos-routing/__tests__/index.test.js
@@ -62,31 +62,31 @@ test('nodos-routing route helpers should return correct url', async () => {
   const article = { id: 123 };
   const comment = { id: 321 };
 
-  const rootPath = router.buildPath('root');
+  const rootPath = router.route('root');
   const expectedRootPath = '/';
   expect(rootPath).toBe(expectedRootPath);
 
-  const apiRootPath = router.buildPath('apiRoot');
+  const apiRootPath = router.route('apiRoot');
   const expectedApiRootPath = '/api';
   expect(apiRootPath).toBe(expectedApiRootPath);
 
-  const apiUsersPath = router.buildPath('apiUsers');
+  const apiUsersPath = router.route('apiUsers');
   const expectedApiUsersPath = '/api/users';
   expect(apiUsersPath).toBe(expectedApiUsersPath);
 
-  const buildArticlesPath = router.buildPath('buildArticle');
+  const buildArticlesPath = router.route('buildArticle');
   const expectedBuildArticlesPath = '/articles/build';
   expect(buildArticlesPath).toBe(expectedBuildArticlesPath);
 
-  const articleCommentsPath = router.buildPath('articleComments', article.id);
+  const articleCommentsPath = router.route('articleComments', article.id);
   const expectedArticleCommentsPath = `/articles/${article.id}/comments`;
   expect(articleCommentsPath).toBe(expectedArticleCommentsPath);
 
-  const articleCommentPath = router.buildPath('articleComment', article.id, comment.id);
+  const articleCommentPath = router.route('articleComment', article.id, comment.id);
   const expectedArticleCommentPath = `/articles/${article.id}/comments/${comment.id}`;
   expect(articleCommentPath).toBe(expectedArticleCommentPath);
 
-  const articleCommentUrl = router.buildUrl('articleComment', article.id, comment.id);
+  const articleCommentUrl = router.route('articleComment', true, article.id, comment.id);
   const expectedArticleCommentUrl = `http://site.com/articles/${article.id}/comments/${comment.id}`;
   expect(articleCommentUrl).toBe(expectedArticleCommentUrl);
 });
