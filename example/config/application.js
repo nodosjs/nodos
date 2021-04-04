e95256094d7d605a44b5741affd03d5228fbd084
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2021-04-04 13:26:07 +0500

    move from require to import

--- example/config/application.js
+++ example/config/application.js
@@ -1,14 +1,12 @@
 import nodosDb from '@nodosjs/db-typeorm-extension';
 import nodosView from '@nodosjs/view-extension';
-// import Inflector from 'inflected';
-// TODO: switch to import
-const Inflector = require('inflected');
+import { inflections } from 'inflected';
 
 export default async (app) => {
   app.addExtension(nodosDb);
   app.addExtension(nodosView);
 
-  Inflector.inflections('en', (inflect) => {
+  inflections('en', (inflect) => {
     inflect.irregular('person', 'people');
   });
 };
