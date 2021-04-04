89e15df94b47916e036270f1e1aea76fbb0a5fc2
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2021-04-05 00:35:56 +0500

    create new db-prisma package

--- packages/nodos-db-prisma-extension/index.js
+++ packages/nodos-db-prisma-extension/index.js
@@ -1,38 +1,37 @@
 const path = require('path');
 const fsp = require('fs/promises');
 const { PrismaClient } = require('@prisma/client');
-// const fastifyObjectionjs = require('fastify-objectionjs');
 
 // const commandBuilders = require('./lib/commands.js');
 // const generators = require('./lib/generators.js');
 // const Db = require('./lib/Db.js');
 const log = require('./lib/logger.js');
 
-const portsMapping = {
-  postgres: 5432,
-  mysql: 3306,
-};
+const buildConnectionURL = ({
+  type,
+  host,
+  username,
+  password,
+  port,
+  database,
+}) => `${type}://${username}:${password}@${host}:${port}/${database}`;
+
 
 module.exports = async (app) => {
   const appConfig = app.config.db || {};
   // const entities = require(path.join(app.config.projectRoot, 'app/entities/index.js')).default;
   const defaultConfig = {
-    port: portsMapping[appConfig.type],
-    synchronize: true, // # FIXME: only development and tests
+    port: process.env.NODOS_DB_PORT,
     host: process.env.NODOS_DB_HOSTNAME || 'localhost',
     type: process.env.NODOS_DB_TYPE,
     username: process.env.NODOS_DB_USERNAME,
     password: process.env.NODOS_DB_PASSWORD,
-    entitiesPath: path.join(app.config.projectRoot, 'app/entities'),
-    // connection: path.join(app.config.projectRoot, 'db/development.sqlite3'),
-    // TODO: подобные штуки не должны переопределяться
-    migrationPaths: [path.join(app.config.projectRoot, 'db/migrations/**/*.js')],
-    logging: true,
-    connectTimeout: 1000,
-    debug: true,
+    database: process.env.NODOS_DB_NAME,
   };
   const config = { ...defaultConfig, ...appConfig };
-  log('init db extension', config);
+  const url = buildConnectionURL(config);
+  const prismaConfig = { datasources: { db: { url } } };
+  log('init db extension', prismaConfig);
 
   const data = {}; // little hack for lazy loading
   // Object.values(commandBuilders).forEach((build) => app.addCommandBuilder(build));
@@ -40,10 +39,9 @@ module.exports = async (app) => {
   // app.addMiddleware(path.resolve(__dirname, './lib/middlewares/handleDbErrors.js'));
   // app.addMiddleware(path.resolve(__dirname, './lib/middlewares/checkMigrations.js'));
   // app.fastify.addHook('onStop', () => db.close());
-  app.fastify.addHook('onClose', () => data.db && data.db.$disconnect());
+  app.fastify.addHook('onClose', () => data.db && data.db.close());
   app.fastify.addHook('onReady', async () => {
-    const prisma = new PrismaClient();
-    // const db = await createConnection(config);
+    const prisma = new PrismaClient(prismaConfig);
     data.db = prisma;
     app.addDependency('db', prisma);
   });
