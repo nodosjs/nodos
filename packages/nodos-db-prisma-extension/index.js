0d574d990345fe1f4001747e41701907cb66ad49
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2021-04-18 01:38:00 +0500

    temporary turn off db configuration by nodos

--- packages/nodos-db-prisma-extension/index.js
+++ packages/nodos-db-prisma-extension/index.js
@@ -7,31 +7,19 @@ const { PrismaClient } = require('@prisma/client');
 // const Db = require('./lib/Db.js');
 const log = require('./lib/logger.js');
 
-const buildConnectionURL = ({
-  type,
-  host,
-  username,
-  password,
-  port,
-  database,
-}) => `${type}://${username}:${password}@${host}:${port}/${database}`;
-
-
 module.exports = async (app) => {
   const appConfig = app.config.db || {};
-  // const entities = require(path.join(app.config.projectRoot, 'app/entities/index.js')).default;
-  const defaultConfig = {
-    port: process.env.NODOS_DB_PORT,
-    host: process.env.NODOS_DB_HOSTNAME || 'localhost',
-    type: process.env.NODOS_DB_TYPE,
-    username: process.env.NODOS_DB_USERNAME,
-    password: process.env.NODOS_DB_PASSWORD,
-    database: process.env.NODOS_DB_NAME,
-  };
-  const config = { ...defaultConfig, ...appConfig };
-  const url = buildConnectionURL(config);
-  const prismaConfig = { datasources: { db: { url } } };
-  log('init db extension', prismaConfig);
+
+  // TODO: we have to decide how we will configure Prisma - nodos config files or schema.prisma file
+  // const defaultConfig = {
+  //   port: process.env.NODOS_DB_PORT,
+  //   host: process.env.NODOS_DB_HOSTNAME || 'localhost',
+  //   type: process.env.NODOS_DB_TYPE,
+  //   username: process.env.NODOS_DB_USERNAME,
+  //   password: process.env.NODOS_DB_PASSWORD,
+  //   database: process.env.NODOS_DB_NAME,
+  // };
+  // const config = { ...defaultConfig, ...appConfig };
 
   const data = {}; // little hack for lazy loading
   // Object.values(commandBuilders).forEach((build) => app.addCommandBuilder(build));
@@ -39,9 +27,9 @@ module.exports = async (app) => {
   // app.addMiddleware(path.resolve(__dirname, './lib/middlewares/handleDbErrors.js'));
   // app.addMiddleware(path.resolve(__dirname, './lib/middlewares/checkMigrations.js'));
   // app.fastify.addHook('onStop', () => db.close());
-  app.fastify.addHook('onClose', () => data.db && data.db.close());
+  app.fastify.addHook('onClose', () => data.db && data.db.$disconnect());
   app.fastify.addHook('onReady', async () => {
-    const prisma = new PrismaClient(prismaConfig);
+    const prisma = new PrismaClient();
     data.db = prisma;
     app.addDependency('db', prisma);
   });
